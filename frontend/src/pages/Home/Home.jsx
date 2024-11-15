13341c4d7a258100a05130b4402ae265d9060d8d
ArshalanQuasain <arshalanquasain.01@gmail.com>
2024-11-15 14:58:39 +0530

    v

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import axiosInstances from '../../utils/axiosinstance';
 import Toast from '../../components/ToastMessage/Toast';
 import EmptyCard from '../../components/emptyCard/EmptyCard';
-import noCar from '../../assets/Images/NoCar.png'
+import noData from '../../assets/Images/noData.jpg'
 
 function Home() {
   const [openAddEditModel, setOpenAddEditModel] = useState({
@@ -149,7 +149,7 @@ function Home() {
           </div>
         ) : (
           <EmptyCard
-            imgSrc={noCar}
+            imgSrc={noData}
             message={isSearch ? `Oops no cars found with your query` : `Start adding your first car! Click the 'Add' button to add a new car listing.`}
           />
         )}
