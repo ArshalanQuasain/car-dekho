8b452b4a43d104d69485d06d45b86bd053c55642
ArshalanQuasain <arshalanquasain.01@gmail.com>
2024-11-15 15:19:29 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: "*", // Replace with your frontend URL
     credentials: true,
 }));
 
